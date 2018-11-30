d7dad7bb3e3275d0e439836521a075466f25f133
Steven Beyer <sbeyerjr@gmail.com>
2018-11-29 19:42:37 -0700

    added some front end testing

--- src/components/PracticeData.js
+++ src/components/PracticeData.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import axios from 'axios';
 import { API_BASE_URL } from '../config';
-import Header from './Header';
 import './MyPractice.css';
 import requiresLogin from './RequiresLogin';
 
